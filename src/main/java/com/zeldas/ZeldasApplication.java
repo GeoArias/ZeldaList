c7564cb73bb276e2f6bde92c3a5c630ca739ed7b
geoas <geoas@TF-008459591733>
2023-10-01 13:33:04 -0600

    Subida del proyecto en Git

---
+++ src/main/java/com/zeldas/ZeldasApplication.java
@@ -0,0 +1,13 @@
+package com.zeldas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeldasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeldasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zeldas/ZeldasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeldas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeldasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
